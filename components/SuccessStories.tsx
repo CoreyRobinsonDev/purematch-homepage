b4765d86c82d56c9a25f584d7cd4171d5b2a4e26
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-12-12 04:14:26 -0500

    added and styled success stories component

--- components/SuccessStories.tsx
+++ components/SuccessStories.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
 
 import styles from "../styles/SuccessStories.module.css";
@@ -7,10 +8,31 @@ const SuccessStories = () => {
     <header className={styles.success_header}>
       <h3 className={styles.header__title}>SUCCESS STORIES</h3>
       <span className={styles.header__arrows}>
-        <button className={styles.success__prev_btn}><BsArrowLeft/></button>
-        <button className={styles.success__next_btn}><BsArrowRight/></button>
+        <button className={styles.success__arrow}><BsArrowLeft/></button>
+        <button className={styles.success__arrow}><BsArrowRight/></button>
       </span>
     </header>
+    <main className={styles.story_container}>
+      <div>
+        <span className={styles.story__quotation_mark}>{'“'}</span>
+        <p className={styles.story__description}>He started a conversation with me and our friendship grew so fast! Now we’re dating, which is wild to me because I got the app just to get to know people. I had no idea that God would bless me with such a cool boyfriend! Super thankful for this app and definitely encourage people to try it out because you never know how God could surprise you!
+        </p>
+        <p className={styles.story__author}><span className={styles["--em_dash"]}>——</span> Chloe and George</p>
+      </div>
+      <div className={styles.success_img_container}>
+        <Image
+          src="/images/success-img-1.png" 
+          alt=""
+          width={299.2}
+          height={398.46}
+        />
+      </div>
+    </main>
+    <footer className={styles.page_indicator_container}>
+      <span className={`${styles.story__page_indicator} ${styles["--current"]}`}></span>
+      <span className={`${styles.story__page_indicator}`}></span>
+      <span className={`${styles.story__page_indicator}`}></span>
+    </footer>
   </section>
 }
 
