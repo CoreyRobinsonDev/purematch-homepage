c69b7e6e034e69d5914cef1d4c8d0b50b11e779b
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-12-13 06:32:38 -0500

    get purematch button and logo now change colors when the success stories component is in view

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaCoins } from "react-icons/fa";
+import { motion, useScroll } from "framer-motion";
+import { useEffect, useState } from "react";
 
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
+  const [y, setY] = useState(0);
+  const { scrollY } = useScroll();
+  const successComponentStart = 1100;
+  const successComponentEnd = 2000;
+
+  useEffect(() => {
+  return scrollY.onChange((latest) => {
+    setY(latest);
+  })
+}, [scrollY])
+
   return <header className={styles.container}>
     <Link className={styles.logo_container} href="/">
-      <Image
-        className={styles.logo__img}
-        src="/images/Pure-Match-Logo.png"
-        alt="Pure Match Logo"
-        width={56}
-        height={56}
+      <motion.img className={styles.logo__img}
+        src="/images/Pure-Match-Logo.png" alt="Pure Match Logo"
+        animate={y >= successComponentStart && y < successComponentEnd ? {filter: "brightness(0) invert(1)"} : {filter: "none"}}
       />
-      <h1 className={styles.logo__name}>Pure Match</h1>
+      <motion.h1 className={styles.logo__name}
+        animate={y >= successComponentStart && y < successComponentEnd ? {color: "white"} : {color: "#0089ff"}}>Pure Match</motion.h1>
     </Link>
     <nav className={styles.nav_container}>
       <ul className={styles.nav__list}>
@@ -23,9 +34,10 @@ const Header = () => {
         <li><Link className={styles.nav__item} href="#">Blog</Link></li>
         <li><Link className={styles.nav__item} href="#">Contact</Link></li>
         <li><Link className={`${styles.nav__item} ${styles.nav__donate_link}`} href="#"><FaCoins/> Donate</Link></li>
-        <li><Link className={`${styles.nav__item} ${styles.nav__purematch_btn}`} href="#">Get PureMatch</Link></li>
+        
       </ul>
     </nav>
+      <motion.a className={`${styles.nav__item} ${styles.nav__purematch_btn}`} href="#" animate={y >= successComponentStart && y < successComponentEnd ? {color: "#0089ff", backgroundColor: "white"} : {color: "white", backgroundColor: "#0089ff"}}>Get PureMatch</motion.a>
   </header>
 }
 
--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -6,8 +6,8 @@ const SignUp = () => {
     <h2 className={styles.header}>Stay connected!</h2>
     <p className={styles.info}>Sign up for our newsletter to get the latest news and updates, as well as exclusive perks and offers!</p>
     <form className={styles.form}>
-      <input className={styles.form__input} type="text" placeholder="Enter your name" required />
-      <input className={styles.form__input} type="email" placeholder="Enter your email" required />
+      <input className={styles.form__input} type="text" placeholder="Enter your name" name="name" required />
+      <input className={styles.form__input} type="email" placeholder="Enter your email" name="email" required />
       <button className={styles.form__submit_btn} type="submit">Subscribe</button>
     </form>
     <small className={styles.small_text}>We respect your privacy.</small>
