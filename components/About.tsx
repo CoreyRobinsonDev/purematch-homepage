1d5023e162e1a8553bd71608548a94314273bda6
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-12-13 05:47:39 -0500

    feature images now change when the user scrolls down different product features

--- components/About.tsx
+++ components/About.tsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
-import { motion } from "framer-motion";
-import { useState, useEffect, useMemo } from "react";
+import { motion, useInView } from "framer-motion";
+import { useRef, useEffect } from "react";
 
 import styles from "../styles/About.module.css";
 
 
 const About = () => {
+  const container = useRef() as React.RefObject<HTMLDivElement>;
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref1IsInView = useInView(ref1, {root: container})
+  const ref2IsInView = useInView(ref2, {root: container})
+  const ref3IsInView = useInView(ref3, {root: container})
+
+  useEffect(() => {
+    if (ref1IsInView) console.log(1)
+    if (ref2IsInView) console.log(2)
+    if (ref3IsInView) console.log(3)
+
+  }, [ref1IsInView, ref2IsInView, ref3IsInView])
+
   return <section className={styles.container}>
     <div className={styles.about__summary}>
       <h2 className={styles.summary__header}>Connect, Date, and Socialize.</h2>
@@ -46,15 +61,35 @@ const About = () => {
         height={569.34}
       />
     </figure>
-    <Image
-      src="/images/Feature-Photo-1.png"
-      alt=""
-      width={448}
-      height={448}
-    />
-    <div className={styles.feature_container}>
-      <h2 className={styles.feature__header}>Connect as a community</h2>
-      <p className={styles.feature__description}>Dating and marriage may not be the sole focus of discipleship, but we all need connection. As Christians, it’s important to have deep relationships with like-minded believers we trust and respect; our app facilities that.</p>
+    <figure className={styles.feature_image_container}>
+      <motion.img className={styles.feature_image} src="/images/Feature-Photo-1.png" alt=""
+        initial={{opacity: 0}}
+        animate={ref1IsInView ? {opacity: 1} : {opacity: 0}}
+      />
+      <motion.img className={styles.feature_image} src="/images/Feature-Photo-2.png" alt=""
+        initial={{opacity: 0}}
+        animate={ref2IsInView ? {opacity: 1} : {opacity: 0}}
+      />
+      <motion.img className={styles.feature_image} src="/images/Feature-Photo-3.png" alt=""
+        initial={{opacity: 0}}
+        animate={ref3IsInView ? {opacity: 1} : {opacity: 0}}
+      />
+      
+    </figure>
+    <div className={styles.feature_container} ref={container}>
+      <motion.article className={styles.feature} ref={ref1}>
+        <h2 className={`${styles.feature__header} ${styles["--1"]}`}>Connect as a community</h2>
+        <p className={styles.feature__description}>Dating and marriage may not be the sole focus of discipleship, but we all need connection. As Christians, it’s important to have deep relationships with like-minded believers we trust and respect; our app facilities that.</p>
+      </motion.article>
+      <motion.article className={styles.feature} ref={ref2}>
+        <h2 className={`${styles.feature__header} ${styles["--2"]}`}>Form organic relationships</h2>
+        <p className={styles.feature__description}>Pure Match at its heart is a place for all disciples (single and married) to connect, network, and socialize in order to strengthen the bond of the kingdom of Heaven on earth.</p>
+      </motion.article>
+      <motion.article className={styles.feature} ref={ref3}>
+        <h2 className={`${styles.feature__header} ${styles["--3"]}`}>Make meaningful conversations</h2>
+        <p className={styles.feature__description}>Pure Match hopes to foster and grow relationships between like-minded disciples in a healthy community. Connect with Christians you have things in common with. Our environment fosters many lasting partnerships such as network connections and even marriage.</p>
+      </motion.article>
+
     </div>
   </section>
 }
